ff73b7ceb7de9dd1ae721a71a40a542ec7afa6d7
Daviduxx <borotto.davide@gmail.com>
2023-09-05 11:45:27 +0200

    initial commit

---
+++ wedive/src/main/java/com/wedive/Spring/WediveApplication.java
@@ -0,0 +1,13 @@
+package com.wedive.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WediveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WediveApplication.class, args);
+	}
+
+}
---
+++ wedive/src/test/java/com/wedive/Spring/WediveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedive.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WediveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
