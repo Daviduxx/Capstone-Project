7c25495f58d4f7794c4b3a147f91a72322529cdf
Daviduxx <borotto.davide@gmail.com>
2023-09-12 23:38:00 +0200

    signup form

--- wedive_frontend/wedive_fe/src/app/components/register/register.component.ts
+++ wedive_frontend/wedive_fe/src/app/components/register/register.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+  signup!: FormGroup;
+
+constructor() {}
+
+ngOnInit(): void {
+
+  this.signup = new FormGroup({
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(15)
+    ]),
+    surname: new FormControl(null),
+    username: new FormControl(null),
+    email: new FormControl(null),
+    birth: new FormControl(null),
+    password: new FormControl(null)
+    }
+  );
+}
+
+
+onSubmit() {
+
+   console.log(this.signup)
+}
 
 }
