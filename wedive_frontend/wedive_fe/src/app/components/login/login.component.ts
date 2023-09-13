b9346896781b6a521a744f8810b098ed86cdf552
Daviduxx <borotto.davide@gmail.com>
2023-09-13 16:05:48 +0200

    validators

--- wedive_frontend/wedive_fe/src/app/components/login/login.component.ts
+++ wedive_frontend/wedive_fe/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/auth.service';
 
 @Component({
@@ -12,20 +12,42 @@ export class LoginComponent implements OnInit{
   constructor( private aSvc: AuthService) {}
 
   login!: FormGroup;
+  error: undefined | string;
 
   ngOnInit(): void {
 
     this.login = new FormGroup({
-      username: new FormControl(null),
-      password: new FormControl(null)
+      username: new FormControl(null,
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(15),
+        ]
+        ),
+      password: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(3)
+      ])
       }
     );
   }
 
   onSubmit() {
+    if(this.login.value.username !== null && this.login.value.password !== null) {
       this.aSvc.login(this.login.value).subscribe(
-        res => console.log(res)
-      )
+        resp => {
+          console.log(resp);
+          this.error = undefined;
+          localStorage.setItem('userLogin', JSON.stringify(resp));
+        }, err => {
+          console.log(err.error.message);
+          this.error = err.error.message;
+        })
+      this.error = undefined;
+  } else {
+    this.error = 'Field Required';
+  }
     }
 
-}
+    }
+
--- wedive_frontend/wedive_fe/src/app/components/register/register.component.ts
+++ wedive_frontend/wedive_fe/src/app/components/register/register.component.ts
@@ -10,6 +10,7 @@ import { AuthService } from 'src/app/auth.service';
 export class RegisterComponent implements OnInit{
 
   signup!: FormGroup;
+  error: undefined | string;
 
 constructor( private aSvc: AuthService ) {}
 
@@ -19,26 +20,61 @@ ngOnInit(): void {
     name: new FormControl(null, [
       Validators.required,
       Validators.minLength(3),
-      Validators.maxLength(15)
+      Validators.maxLength(15),
     ]),
-    surname: new FormControl(null),
-    username: new FormControl(null),
-    email: new FormControl(null),
+    surname: new FormControl(null,
+      [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(25),
+      ]
+      ),
+    username: new FormControl(null,
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(15),
+      ]),
+    email: new FormControl(null,
+      [
+        Validators.required,
+       Validators.email,
+       Validators.pattern("^[a-z0-9.]{3,20}@[a-z]{2,7}\\.[a-z]{2,5}$")
+      ]
+      ),
     birthday: new FormControl(null),
-    password: new FormControl(null)
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3)
+    ]
+      )
     }
   );
 }
 
 
 onSubmit() {
-
-   console.log(this.signup);
-  this.aSvc.signup(this.signup.value).subscribe(
-    res => console.log(res),
-    err => console.log(err)
-
-  )
-}
-
+  if(this.signup.value.name !== null
+    && this.signup.value.surname !== null
+    && this.signup.value.username !== null
+    && this.signup.value.email !== null
+    && this.signup.value.birthday !== null
+    && this.signup.value.password !== null
+    ) {
+      this.aSvc.signup(this.signup.value).subscribe(
+        res => {
+          console.log(res);
+          this.error = undefined;
+        },
+        err => {
+          console.log(err.error.message);
+          this.error = err.error.message;
+        }
+          );
+        }
+        else {
+          this.error = "Field required!"
+          console.error(this.error);
+        }
+    }
 }
