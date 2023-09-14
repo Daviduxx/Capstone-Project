f980776850b581fdd9a89cd150c82cde7669851e
Daviduxx <borotto.davide@gmail.com>
2023-09-14 15:36:18 +0200

    hero section

--- wedive_frontend/wedive_fe/src/app/homepage/homepage.module.ts
+++ wedive_frontend/wedive_fe/src/app/homepage/homepage.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage.component';
+import { HerosectionComponent } from './components/herosection/herosection.component';
 
 
 @NgModule({
   declarations: [
-    HomepageComponent
+    HomepageComponent,
+    HerosectionComponent
   ],
   imports: [
     CommonModule,
