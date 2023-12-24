7e88d6ac20c38632905563a9ebe757133b73c932
Daviduxx <borotto.davide@gmail.com>
2023-12-24 11:52:35 +0100

    on viewport component
    The "stats" component now begin thew setInterval function only if visible on the viewport

--- wedive_frontend/wedive_fe/src/app/homepage/components/stats/stats.component.ts
+++ wedive_frontend/wedive_fe/src/app/homepage/components/stats/stats.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Observable, Subscription, timer } from 'rxjs';
 
 @Component({
@@ -6,35 +6,62 @@ import { Observable, Subscription, timer } from 'rxjs';
   templateUrl: './stats.component.html',
   styleUrls: ['./stats.component.scss']
 })
-export class StatsComponent implements AfterViewInit{
+export class StatsComponent{
 
    divers: number = 0;
    dives: number = 0;
    divingCenters: number = 0;
 
-   // this doesn't work. Need to find how to load this components only if visible in the viewport
-  ngAfterViewInit(): void {
-    console.log('view init');
+   diversCounter!:any;
+   divesCounter!:any;
+   divingCenterCounter!:any;
+
+  diversVisibility:boolean = false;
+  divesVisibility:boolean = false;
+  divingCentersVisibility:boolean = false;
+
+  @ViewChild('diversBox') diversNumber!: ElementRef;
+  @ViewChild('divesBox') divesNumber!: ElementRef;
+  @ViewChild('divingCenterBox') divingCenterNumber!: ElementRef;
+
+  @HostListener('document:scroll', ['$event'])
+  public onViewportScroll() {
+    const windowHeight = window.innerHeight;
+    console.log(windowHeight)
+    const boundingRectDivers = this.diversNumber.nativeElement.getBoundingClientRect();
+    console.log(boundingRectDivers)
+    if(boundingRectDivers.top >= 0 && boundingRectDivers.bottom <= windowHeight){
+
+      this.diversVisibility = true;
+      this.diversCounter = setInterval(() => {
+        this.divers++;
+        if(this.divers >= 200){
+          clearInterval(this.diversCounter);
+          this.divers = 200;
+        }
+      },60)
+
+      this.divesVisibility = true;
+      this.divesCounter = setInterval(() => {
+        this.dives++;
+        if(this.dives >= 1500){
+          clearInterval(this.divesCounter)
+          this.dives = 1500;
+        }
+      },2)
+
+      this.divingCentersVisibility = true;
+      this.divingCenterCounter = setInterval(() => {
+        this.divingCenters++;
+        if(this.divingCenters >= 120){
+          clearInterval(this.divingCenterCounter)
+          this.divingCenters = 120;
+        }
+      },50)
+    }
   }
 
-  diversCounter = setInterval(() => {
-    this.divers++;
-    if(this.divers === 200){
-      clearInterval(this.diversCounter)
-    }
-  },30)
 
-  divesCounter = setInterval(() => {
-    this.dives++;
-    if(this.dives === 1500){
-      clearInterval(this.divesCounter)
-    }
-  },2)
 
-  divingCenterCounter = setInterval(() => {
-    this.divingCenters++;
-    if(this.divingCenters === 120){
-      clearInterval(this.divingCenterCounter)
-    }
-  },50)
+
 }
